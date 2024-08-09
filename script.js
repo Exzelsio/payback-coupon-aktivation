f70b922d09341ffc73dcb4d784d66549e0a64656
Florian <102511744+Exzelsio@users.noreply.github.com>
2024-08-09 13:30:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+console.clear();
+
+(function() {
+    let timeout = 0;
+    const couponCenter = document.querySelector('pb-coupon-center');
+
+    if (couponCenter && couponCenter.shadowRoot) {
+        const coupons = couponCenter.shadowRoot.querySelectorAll('pbc-coupon');
+
+        coupons.forEach((coupon) => {
+            const callToAction = coupon.shadowRoot.querySelector('pbc-coupon-call-to-action');
+            if (callToAction && callToAction.shadowRoot) {
+                const button = callToAction.shadowRoot.querySelector('button.not-activated');
+                
+                if (button) {
+                    setTimeout(() => {
+                        button.click();
+                    }, 100 * timeout);
+                    timeout += 1;
+                }
+            }
+        });
+    } else {
+        console.warn('pb-coupon-center element or its shadowRoot not found.');
+    }
+})();
