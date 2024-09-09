5ef6d66a0303b481530a0d2cc86b2700d74cfd79
Florian <102511744+Exzelsio@users.noreply.github.com>
2024-09-09 21:16:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-console.clear();
-
 (function() {
     let timeout = 0;
     const couponCenter = document.querySelector('pb-coupon-center');
 
     if (couponCenter && couponCenter.shadowRoot) {
         const coupons = couponCenter.shadowRoot.querySelectorAll('pbc-coupon');
-
         coupons.forEach((coupon) => {
             const callToAction = coupon.shadowRoot.querySelector('pbc-coupon-call-to-action');
             if (callToAction && callToAction.shadowRoot) {
@@ -20,7 +17,5 @@ console.clear();
                 }
             }
         });
-    } else {
-        console.warn('pb-coupon-center element or its shadowRoot not found.');
     }
 })();
